bca14729571d8431dab35744936887ba7addd39c
FrancisSilo <148843750+FrancisSilo@users.noreply.github.com>
2023-10-24 22:01:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + "  | "
+    saveEl.innerHTML += "<p>" + countStr + "</p>"
+    
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    saveEl.textContent = ""
+    saveEl.textContent = "Previous entries: "
+}
